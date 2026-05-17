ac08dd40e7c122c4b0f0bd296d0d7b44b89ba71e
Parth Parikh <parth.parikh@my.bdsc.school.nz>
2026-05-17 22:54:59 +1200

    Initialising the main dictionary holding the tasks/Lists

--- main_v1.py
+++ main_v1.py
@@ -1,2 +1,5 @@
 '''This is my To-Do list Application which is the minimum vaible product
 This program allows users to add and manage daily tasks'''
+
+#Initialise the main dictionary which contains all the different tasks and their categories/lists
+task_lists = {}
