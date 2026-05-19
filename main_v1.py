7f30ed50a1ebff4ea6a73b5c275e16fdc3fcd163
Parth Parikh <parth.parikh@my.bdsc.school.nz>
2026-05-20 08:54:49 +1200

    coding what each option in the main menus purpose

--- main_v1.py
+++ main_v1.py
@@ -4,21 +4,29 @@ This program allows users to add and manage daily tasks'''
 #Initialise the main dictionary which contains all the different tasks and their categories/lists
 task_lists = {}
 
-options = ["1.Create List", "2.Delete Lists", "3.Open Lists", "4.Save & Exit"]
+main_menu_options = ["1.Create List", "2.Delete Lists", "3.Open Lists", "4.Save & Exit"]
 
 #Main menu function for the To-Do list app - displays the options
 def main():
     print("Main Menu")
-    for option in options:
+    for option in main_menu_options:
             print(option)
     user_main_menu_choice = int(input("Choose an option from (1,2,3,4):" ))
     if user_main_menu_choice == 1:
         print("Menu Option chosen: Create List")
-    if user_main_menu_choice == "2":
+    elif user_main_menu_choice == "2":
         print("Menu Option chosen: Delete Lists ")
-    if user_main_menu_choice == "3":
+    elif user_main_menu_choice == "3":
         print("Menu Option chosen: Open List ")
-    if user_main_menu_choice == "4":
+    elif user_main_menu_choice == "4":
         print("Menu Option chosen: Save & Exit ")
+    else:
+        print("Your answer was invalid please enter a number from 1,2,3,4 correctly")
 
-main()
+def create_list(): #adds a list that the user creates to the main dictionary of the catogries
+
+def delete_lists(): #Deletes a list
+
+def open_list(): #Displays a current list/category that already exist in the dictionary  
+
+def save_exit(): #programs closes
