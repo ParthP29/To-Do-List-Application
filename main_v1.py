a7dd730d947407a3beeb3ff6ec93740aad2b2b2e
Parth Parikh <parth.parikh@my.bdsc.school.nz>
2026-05-25 21:42:57 +1200

    coded the save_exit_write_file function where i can save all the data to reuse

--- main_v1.py
+++ main_v1.py
@@ -1,19 +1,33 @@
 '''This is my To-Do list Application which is the minimum vaible product
 This program allows users to add and manage daily tasks'''
-
+#import module to save the user data
+import json
 #Initialise the main dictionary which contains all the different tasks and their categories/lists
 task_lists = {}
 
+#The external file to open to write, read, and append to
+FILENAME = "to_do_save_dictionary.txt"
+#The menus lists with all options
 main_menu_options = ["1.Create List", "2.Delete Lists", "3.Open Lists", "4.Save & Exit"]
 second_menu_options = ["1.Show Task","2.Create Tasks","3.Mark Tasks Complete","4.Remove Tasks","5.Go Back"]
 
 print("Welcome to To-Do List Application") #welcome statement
 
+
+def load_previous_data(): 
+    with open(FILENAME, "r") as f: #uses the json module that was imported and opens the file to read the info to use the variable in the main code.
+        return json.load(f)
+    
+#if user wants to load previous data it saves the data as the main dictionary
+load_user_choice = input("Do you want to load prevous saved data/continue your session from before")
+if load_user_choice == 'yes':
+    task_lists = load_previous_data() #Inialises the main dicationary as the dict that was saved in the previous program runs with the previous data/lists/tasks. 
+
 #Main menu function for the To-Do list app - displays the options
 def main():
     while True:
         print("Main Menu")
-        for option in main_menu_options:
+        for option in main_menu_options:  #loops through options for the main menu and displays them
                 print(option)
         user_main_menu_choice = int(input("Choose an option from (1,2,3,4):" ))
         if user_main_menu_choice == 1:
@@ -27,7 +41,7 @@ def main():
             open_list()
         elif user_main_menu_choice == 4:
             print("Menu Option chosen: Save & Exit ")
-            save_exit()
+            save_exit_write_file()
         else:
             print("Your answer was invalid please enter a number from 1,2,3,4 correctly")
 
@@ -35,7 +49,7 @@ def create_list(): #adds a list that the user creates to the main dictionary of 
     print("Create a category/list for your tasks to fall under") 
     name_list = input("Create a name for the list: ")
     
-    #Create a new emtpy list inside the dictionary
+    #Creates  a new emtpy list inside the dictionary
     task_lists[name_list] = []
 
     print(f'Your list {name_list} has been created') #tells user that list is acutally created
@@ -56,7 +70,9 @@ def delete_lists(): #Deletes a list
     print(f'your list, is deleted')
     
 
-def open_list(): #Displays a current list/category that already exist in the dictionary  
+#Displays a current list/category that already exist in the dictionary
+#Then display the second menu for task options (for eg. you can mark tasks)
+def open_list():    
     print("Open a current list/category to view its opions")
     for i, category in enumerate(task_lists.keys(), start=1):
         print(f"{i}.{category}")
@@ -69,7 +85,7 @@ def open_list(): #Displays a current list/category that already exist in the dic
     
     while True:
         print("Task Menu")
-        for option in second_menu_options:
+        for option in second_menu_options: #loops through options in the second task menu lists and displays them
                 print(option)
         user_task_menu_choice = int(input("Choose an option from (1,2,3,4):" ))
         if user_task_menu_choice == 1:
@@ -82,14 +98,14 @@ def open_list(): #Displays a current list/category that already exist in the dic
                     print(f'- {tasks}')
                     #display tasks if there is any
 
-        elif user_task_menu_choice == 2:
+        elif user_task_menu_choice == 2:  #add tasks/adds values to an individual catorgy list that was chosen
             print("Task menu Option chosen: Create Tasks ")
             #Create new tasks in the list chosen
             task = input("enter the task you want to add")
             task_lists[list_name].append(task)
             print(f"{task} task is added to {list_name}")
 
-        elif user_task_menu_choice == 3:
+        elif user_task_menu_choice == 3: #marks the tasks complete so when viewing open lists to view tasks it shows complete
             print("Task menu Option chosen: Mark tasks you've completed")
             #mark item complete
             print("Tasks:")
@@ -117,36 +133,16 @@ def open_list(): #Displays a current list/category that already exist in the dic
             break
         else:
             print("Your answer was invalid please enter a number from 1,2,3,4 correctly")
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-    
-
-
-
-
-def save_exit(): #programs closes
-  print("Goodbye")  
-
-'''#My second menu when you choose to open a list, then for example choose to mark tasks complete
-def list_menu(): #second menu
-    
-def add_task(): #add tasks/adds values to an individual catorgy list that was chosen
-     
-def mark_task_complete(): #marks the tasks complete so when viewing open lists to view tasks it show complete
-     
-def view_tasks():
-     
-def return_main_menu(): #goes to main menu '''
+def save_exit_write_file(): #programs closes and saves by writing contents to file
+  #saves dictionary and lists data to save all the previous tasks and categories for the lists
+  save_data = input("Do you want to save your data")
+  if save_data == "yes":
+      print('HI')
+      with open(FILENAME, "w") as f:  #opens the save dictioanry file and overwrites the main variable dictionary 'task_lists' into the file.
+          json.dump(task_lists, f)
+  else:
+        print("Goodbye")  
 
 main()
      
